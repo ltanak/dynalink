9f7fcefe6d35a898db1a7350de90ce0817d6dde4
ltanak <ltanakbusiness@outlook.com>
2026-02-14 13:22:53 +0000

    Wip: stricter contract enforcement

--- src/dynalink/decorators.py
+++ src/dynalink/decorators.py
@@ -1,5 +1,7 @@
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Any
 from dynalink.core import registry
+import inspect
+
 print("Step 2: get decorator")
 
 T = TypeVar("T") # generics in python
@@ -13,9 +15,20 @@ def plugin(name: str, kind: str) -> Callable[[T], T]:
     :rtype: Callable[[T], T]
     """
     def decorator(object: T) -> T:
+        if kind == "callable":
+            _validate_callable(name, object)
         registry.register(name, kind, object)
         return object
     
     return decorator
+
+def _validate_callable(name: str, obj: Any):
+    if not callable(obj):
+        raise TypeError(f"Object '{name}' is not of type callable, when it is expected to be a callable.")
+    
+    signature = inspect.signature(obj)
+    params = signature.parameters
+    return_type = signature.return_annotation
+
 
 print("Step 3: end decorator")
